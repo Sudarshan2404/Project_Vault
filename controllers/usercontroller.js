2ce232621cd67af32d9b970ae00d04b028f9eefd
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-19 16:24:15 +0530

    jsut fixed some errors

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -9,21 +9,30 @@ export const register = async (req, res) => {
   try {
     const { name, username, email, password, avtar, bio } = req.body;
 
-    const userfound = User.findOne({ email });
+    const userfound = await User.findOne({ email });
 
     if (userfound) {
       return res.status(400).json({ message: "User already exist" });
     }
 
-    const newUser = User.create({
+    const hashedPass = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
       name,
       username,
       email,
-      password,
+      password: hashedPass,
       avtar,
       bio,
     });
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      token: genreateToken(newUser._id),
+    });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "An internal server error occured" });
   }
 };
