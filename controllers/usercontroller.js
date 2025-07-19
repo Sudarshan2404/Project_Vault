18f227d0a38a6e855f46aabdfedf6f2854356545
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-19 17:10:38 +0530

    Login routes done

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -10,9 +10,16 @@ export const register = async (req, res) => {
     const { name, username, email, password, avtar, bio } = req.body;
 
     const userfound = await User.findOne({ email });
+    const checkusername = await User.findOne({ username });
 
     if (userfound) {
       return res.status(400).json({ message: "User already exist" });
+    }
+
+    if (checkusername) {
+      return res.status(400).json({
+        message: "Username already exist choose a different username",
+      });
     }
 
     const hashedPass = await bcrypt.hash(password, 10);
@@ -34,5 +41,34 @@ export const register = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "An internal server error occured" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const findUser = await User.findOne({ username });
+    const ismatch = bcrypt.compare(password, findUser.password);
+
+    if (!findUser) {
+      return res
+        .status(403)
+        .json({ message: "User not found please Register" });
+    }
+
+    if (!ismatch) {
+      return res.status(403).json({
+        message: "Invalid Password",
+      });
+    }
+
+    res.status(200).json({
+      _id: findUser._id,
+      token: genreateToken(findUser._id),
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
