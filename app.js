9c5b488f368d6a7b169c83d4ee0d12cf9ec0c138
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-23 12:01:20 +0530

    sepreated auth and users

--- app.js
+++ app.js
@@ -3,28 +3,22 @@ import { connectDB } from "./config/db.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import userRoute from "./routes/Userroute.js";
+import authRoute from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import authMiddleware from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 const app = Express();
 const port = 3000;
+connectDB();
 
 app.use(Express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-connectDB();
-
-app.use("/api/auth", userRoute);
-
-app.get("/protected", authMiddleware, (req, res) => {
-  res.status(200).json({
-    message: "yoho token came",
-    user: req.user,
-  });
-});
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
-import { getme } from ".././controllers/usercontroller.js";
+import { register, login } from ".././controllers/authController.js";
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/getme", authMiddleware, getme);
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
