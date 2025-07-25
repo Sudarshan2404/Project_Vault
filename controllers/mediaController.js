530fad399980a5fb3ce6922d91a5693be7c76e1d
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-25 22:06:26 +0530

    Added avtar logic

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,4 +1,5 @@
 import cloudinary from ".././config/cloudinaryConfig.js";
+import User from "../models/Users.js";
 import fs from "fs";
 
 export const uploadSs = async (req, res) => {
@@ -28,5 +29,39 @@ export const uploadSs = async (req, res) => {
       message: "Internal server Error",
       success: false,
     });
+  }
+};
+
+export const uploadAvtar = async (req, res) => {
+  try {
+    if (!req.files || !req.files.image) {
+      return res
+        .status(400)
+        .json({ success: false, message: "NO file uploaded" });
+    }
+
+    const file = req.files?.image;
+
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      folder: "Projectvault_Avtar",
+    });
+
+    fs.unlinkSync(file.tempFilePath);
+    const avtar = result.secure_url;
+
+    const updateAvtar = await User.findByIdAndUpdate(
+      req.user.id,
+      { avtar: avtar },
+      {
+        new: true,
+      }
+    );
+
+    res
+      .status(203)
+      .json({ success: true, message: "Avtar Added successfully" });
+  } catch (error) {
+    console.error("Error while uploading avtar", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
--- routes/mediaRoute.js
+++ routes/mediaRoute.js
@@ -1,9 +1,10 @@
-import { uploadSs } from ".././controllers/mediaController.js";
+import { uploadAvtar, uploadSs } from ".././controllers/mediaController.js";
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/ss/upload", authMiddleware, uploadSs);
+router.post("/avtar/upload", authMiddleware, uploadAvtar);
 
 export default router;
